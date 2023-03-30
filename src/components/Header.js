50f2745937cb8337b78532beac4e8c29359acf6e
Your Name <mash.pogonina2014@yandex.ru>
2023-03-30 22:05:05 +0300

    fix: едактирование header

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,6 +6,10 @@ function Header() {
     return (
         <header className="header">
             <img src={Logo} alt="Логотип" className="header__logo" />
+            <div className="header__info">
+                {/* <p className="header__email">email@mail.com</p> */}
+                <button className="header__button">Войти</button>
+            </div>
         </header>
     );
 }
